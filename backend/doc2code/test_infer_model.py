dc57640417eac48b398b6d3b9f30f7ed35f1abe7
FredQ <fqin987@gmail.com>
2021-05-13 22:43:18 -0700

    frededits

---
+++ backend/doc2code/test_infer_model.py
@@ -0,0 +1,6 @@
+from src import infer_model
+import re
+escaped = r'"""' + "argument: a list, L \n returns: the sum of all elements in the list" + r'"""'
+
+print(escaped)
+print(infer_model.infer(prompt = escaped))
